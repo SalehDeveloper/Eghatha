ceff01b3aa90922beb86c577e79fdaa92a484782
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-01-22 11:04:12 +0300

    feat(domain):add IUnitOfWork interface with CompleteAsync method

---
+++ Eghatha.Domain/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Domain.Abstractions
+{
+    public interface IUnitOfWork
+    {
+        Task CompleteAsync(CancellationToken cancellationToken);
+    }
+}
