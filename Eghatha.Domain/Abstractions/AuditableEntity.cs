fa81cb0ee9009984c229ee9ee9d225047ac9b1ca
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-01-22 11:02:42 +0300

    feat(domain):add AuditableEntity class

---
+++ Eghatha.Domain/Abstractions/AuditableEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Domain.Abstractions
+{
+    public abstract class AuditableEntity: Entity
+    {
+        protected AuditableEntity()
+        {
+            
+        }
+
+        protected AuditableEntity(Guid id)
+            :base(id)
+        {
+            
+        }
+
+        public DateTime CreatedAt { get; protected set; }
+        public string? CreatedBy { get; protected set; }
+
+
+        public DateTime LastModifiedUtc { get; protected set; }
+        public string? LastModifiedBy { get; protected set; }
+
+
+    }
+}
