6ced25e9ad30ed11796f2024104e6382dac81be1
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-01-23 23:37:44 +0300

    feat(domain):update GeoLocation code by adding Create Method

--- Eghatha.Domain/Shared/ValueObjects/GeoLocation.cs
+++ Eghatha.Domain/Shared/ValueObjects/GeoLocation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ErrorOr;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,18 +9,32 @@ namespace Eghatha.Domain.Shared.ValueObjects
 {
     public record GeoLocation
     {
-        public double Latitude { get; init; }
-        public double Longitude { get; init; }
+        public double Latitude { get; private set; }
+        public double Longitude { get; private set; }
 
-        public GeoLocation(double latitude, double longitude)
+        
+        private GeoLocation(double latitude, double longitude)
         {
-            if (latitude < -90 || latitude > 90)
-                throw new ArgumentOutOfRangeException(nameof(latitude), "Latitude must be between -90 and 90");
-            if (longitude < -180 || longitude > 180)
-                throw new ArgumentOutOfRangeException(nameof(longitude), "Longitude must be between -180 and 180");
+     
 
             Latitude = latitude;
             Longitude = longitude;
+        }
+
+
+        public ErrorOr<GeoLocation> Create ( double latitude, double longitude)
+        {
+            if (latitude < -90 || latitude > 90)
+                Error.Validation(
+                    code:"GeoLocation.InvalidLatitude",
+                    description:"Latitude must be between -90 and 90");
+
+            if (longitude < -180 || longitude > 180)
+              Error.Validation(
+                  code: "GeoLocation.Invalidlongitude",
+                  "Longitude must be between -180 and 180");
+
+            return new GeoLocation(latitude, longitude);
         }
     }
 }
