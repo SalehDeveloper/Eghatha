e31283f3ebabce096589a9e51b388e7520b7c748
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-01-23 19:46:30 +0300

    feat(domain):add Volunteer status [Availabe , UnAvailable , Busy]

---
+++ Eghatha.Domain/Volunteers/VolunteerStatus.cs
@@ -0,0 +1,20 @@
+﻿using Ardalis.SmartEnum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Domain.Volunteers
+{
+    public class VolunteerStatus : SmartEnum<VolunteerStatus>
+    {
+        public static readonly VolunteerStatus Available = new VolunteerStatus(nameof(Available), 1);
+        public static readonly VolunteerStatus UnAvailable = new VolunteerStatus(nameof(UnAvailable), 2);
+        public static readonly VolunteerStatus Busy = new VolunteerStatus(nameof(Busy), 3);
+        public VolunteerStatus(string name, int value) 
+            : base(name, value)
+        {
+        }
+    }
+}
