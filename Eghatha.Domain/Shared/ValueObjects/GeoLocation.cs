1a432420c9ed36944e21a685191609f7194f82e9
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-01-23 23:07:54 +0300

    feat(domain): add GeoLocation record to Shared/ValueObjects Folder

---
+++ Eghatha.Domain/Shared/ValueObjects/GeoLocation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Domain.Shared.ValueObjects
+{
+    public record GeoLocation
+    {
+        public double Latitude { get; init; }
+        public double Longitude { get; init; }
+
+        public GeoLocation(double latitude, double longitude)
+        {
+            if (latitude < -90 || latitude > 90)
+                throw new ArgumentOutOfRangeException(nameof(latitude), "Latitude must be between -90 and 90");
+            if (longitude < -180 || longitude > 180)
+                throw new ArgumentOutOfRangeException(nameof(longitude), "Longitude must be between -180 and 180");
+
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+    }
+}
