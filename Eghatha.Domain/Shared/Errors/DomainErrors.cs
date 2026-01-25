bafc2863ea77b79c410ad7155f5bb9583bcb1cae
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-01-25 19:30:06 +0300

    feat(domain):add domainErrors class

---
+++ Eghatha.Domain/Shared/Errors/DomainErrors.cs
@@ -0,0 +1,17 @@
+﻿using ErrorOr;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Domain.Shared.Errors
+{
+    public static class DomainErrors
+    {
+        public static readonly Error UserIdRequired = Error.Validation(
+            code: "User.UserId.Required",
+            description: "UserId is required .");
+    }
+}
