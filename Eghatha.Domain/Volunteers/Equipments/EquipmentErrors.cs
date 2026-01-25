2d80f0b0862de0fd7e6b3cd891b99026f44514b2
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-01-25 19:29:43 +0300

    feat(domain):add EquipmentErrors class

---
+++ Eghatha.Domain/Volunteers/Equipments/EquipmentErrors.cs
@@ -0,0 +1,43 @@
+﻿using ErrorOr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Domain.Volunteers.Equipments
+{
+    public static class EquipmentErrors
+    {
+        public static readonly Error IdRequired = Error.Validation(
+            code: "Equipment.IdRequired",
+            description: "equipment id is required");
+
+        public static readonly Error NameRequired = Error.Validation(
+          code: "Equipment.NameRequired",
+          description: "equipment name is required");
+
+
+        public static readonly Error UnSupportedCategory = Error.Validation(
+          code: "Equipment.UnSupportedCategory",
+          description: "equipment category unsupported");
+
+        public static readonly Error InvalidStatus = Error.Validation(
+         code: "Equipment.InvalidStatus",
+         description: "invalid requpment status ");
+
+
+        public static readonly Error QuantityShouldBeGreaterThanZero = Error.Validation(
+          code: "Equipment.QuantityShouldBeGreaterThanZero",
+          description: "equipment quantity should be greater than zero");
+
+        public static readonly Error AlreadyDeleted = Error.Conflict(
+            code: "Equipment.AlreadyDeleted",
+            description: "equipemtn already deleted");
+
+        public static readonly Error NotFound = Error.NotFound(
+            code: "Equipment.NotFound",
+            description: "equipment not found");
+        
+    }
+}
