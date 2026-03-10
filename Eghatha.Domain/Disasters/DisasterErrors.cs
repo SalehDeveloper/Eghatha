9221c5950b0fc4a74611d9b9aaea3473c6e77bd3
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-03-10 18:34:27 +0300

    feat(domain): add DisasterError , DIsasterStatus classes

---
+++ Eghatha.Domain/Disasters/DisasterErrors.cs
@@ -0,0 +1,45 @@
+﻿using ErrorOr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Domain.Disasters
+{
+    public static class DisasterErrors
+    {
+        public static Error ReporterNameRequired = Error.Validation(
+            code: "Disaster.ReporterNameRequired",
+            description: "reporter name is required");
+
+        public static Error ReporterContactRequired = Error.Validation(
+            code: "Disaster.ReporterContactRequired",
+            description: "reporter contact is required");
+
+        public static Error ReporterIdRequired = Error.Validation(
+              code: "Disaster.ReporterIdRequired",
+            description: "reporter Id is required");
+
+        public static Error TitleRequired = Error.Validation(
+           code: "Disaster.TitleRequired",
+            description: "title is required");
+
+        public static Error DescriptionRequired = Error.Validation(
+        code: "Disaster.DescriptionRequired",
+         description: "description is required");
+
+        public static Error LocationRequired = Error.Validation(
+        code: "Disaster.LocationRequired",
+         description: "location is required");
+
+        public static Error ReporterInfoRequired = Error.Validation(
+       code: "Disaster.ReporterInfoRequired",
+        description: "reporter info is required");
+
+        
+
+
+
+    }
+}
---
+++ Eghatha.Domain/Disasters/DisasterStatus.cs
@@ -0,0 +1,25 @@
+﻿using Ardalis.SmartEnum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Domain.Disasters
+{
+    public class DisasterStatus : SmartEnum<DisasterStatus>
+    { 
+        public static readonly DisasterStatus InProgress = new DisasterStatus(nameof(InProgress), 1);
+
+        public static readonly DisasterStatus Resolved = new DisasterStatus(nameof(Resolved),2);
+
+        public static readonly DisasterStatus Closed = new DisasterStatus(nameof(Closed),3);
+
+        public static readonly DisasterStatus Reported = new DisasterStatus(nameof(Reported), 4);
+
+        public DisasterStatus(string name, int value) 
+            : base(name, value)
+        {
+        }
+    }
+}
