6d7e58cfe5676e58cb655808cfca34cd04f3a5f3
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-03-13 12:40:29 +0300

    feat(domain): add Report record

---
+++ Eghatha.Domain/Disasters/Reports/Report.cs
@@ -0,0 +1,57 @@
+﻿using ErrorOr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Domain.Disasters.Reports
+{
+    public sealed record Report
+    {
+        public string Summary { get; private set ; }
+
+        public string Teams { get; private set; }
+
+        public string Resources { get; private set; }   
+
+        public string AffectedPersons { get; private set; } 
+
+        public DateTimeOffset IssuedAt { get; private set; }
+
+        private Report() { }
+
+        private Report(string summary, string teams, string resources, string affectedPersons , DateTimeOffset issuedAt)
+        {
+            Summary = summary;
+            Teams = teams;
+            Resources = resources;
+            AffectedPersons = affectedPersons;
+            IssuedAt = issuedAt;
+        }
+
+
+        public static ErrorOr<Report> Create(string summary, string teams, string resources, string affectedPersons , DateTimeOffset issuedAt)
+        {
+            if (string.IsNullOrEmpty(summary))
+            {
+                return Error.Validation("Report.Summary", "Summary is required.");
+            }
+            if (string.IsNullOrEmpty(teams))
+            {
+                return Error.Validation("Report.Teams", "Teams information is required.");
+            }
+            if (string.IsNullOrEmpty(resources))
+            {
+                return Error.Validation("Report.Resources", "Resources information is required.");
+            }
+            if (string.IsNullOrEmpty(affectedPersons))
+            {
+                return Error.Validation("Report.AffectedPersons", "Affected persons information is required.");
+            }
+            return new Report(summary, teams, resources, affectedPersons , issuedAt);
+            
+            
+        }
+    }
+}
