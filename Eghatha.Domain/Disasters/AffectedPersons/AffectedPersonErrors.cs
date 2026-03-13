3f958ee469399bdc482d181a18d5bb47c9f677b8
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-03-13 12:37:58 +0300

    feat(domain):add AffectedPerson entity with HealthStatus and AffectedPersonErrors classes

---
+++ Eghatha.Domain/Disasters/AffectedPersons/AffectedPersonErrors.cs
@@ -0,0 +1,22 @@
+﻿using ErrorOr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Domain.Disasters.AffectedPersons
+{
+    public static class AffectedPersonErrors
+    {
+        public static readonly Error NameRequired = Error.Validation(
+            code: "AffectedPerson.Name.Required",
+            description: "Name is required.");
+
+        public static readonly Error InvalidAge = Error.Validation(
+           code: "AffectedPerson.Age.Invalid",
+          description: "age should be greater than zero.");
+
+
+    }
+}
