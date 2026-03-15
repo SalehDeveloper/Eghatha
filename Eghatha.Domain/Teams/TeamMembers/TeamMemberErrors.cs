1e41d9a31edfd067192098771c3af1c24812b3ac
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-03-16 00:41:36 +0300

    feat(domain): add TeamMember entity with TeamMemberErrors and TeamMemberStatus

---
+++ Eghatha.Domain/Teams/TeamMembers/TeamMemberErrors.cs
@@ -0,0 +1,34 @@
+﻿using Ardalis.SmartEnum;
+using ErrorOr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Domain.Teams.TeamMembers
+{
+    public class TeamMemberErrors : SmartEnum<TeamMemberErrors>
+    {
+        public static readonly Error JobTitleRequired= Error.Validation(
+            code: "TeamMember.JobTitleRequired",
+            description: "Job title is required.");
+
+        public static Error CannotSetToActiveWhenInMission = Error.Conflict(
+       code: "TeamMember.Status.CannotSetToActiveWhenInMission",
+       description: "Cannot set team-member status to active when it's on a mission.");
+
+
+        public static Error StatusRequired = Error.Validation(
+            code: "TeamMemberErrors.Status.Required",
+            description: "Team-Member status is required.");
+
+        public static Error InvalidStatus = Error.Validation(
+           code: "TeamMemberErrors.Status.Invalid",
+           description: "Team-Member status is invalid.");
+
+        public TeamMemberErrors(string name, int value) : base(name, value)
+        {
+        }
+    }
+}
