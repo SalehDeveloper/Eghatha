1be82a920a0d2b89108a72f78f833a570a842d81
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-03-19 01:42:35 +0300

    Feature/identity
    * feat(domin) : add RefreshToken class with RefreshTokenErros
    
    * feat(domain) : add Roles as enum with values [Admin , Volunteer , TeamMember]

---
+++ Eghatha.Domain/Identity/RefreshTokenErrors.cs
@@ -0,0 +1,26 @@
+﻿using ErrorOr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Domain.Identity
+{
+    public static class RefreshTokenErrors
+    {
+        public static readonly Error TokenRequired =
+         Error.Validation(
+             code:"RefreshToken_Token_Required",
+             description:"Token value is required.");
+
+        public static readonly Error ExpiryInvalid =
+        Error.Validation(
+            code:"RefreshToken_Expiry_Invalid",
+            description:"Expiry must be in the future.");
+
+        public static readonly Error TokenAlreadyRevoked =Error.Conflict(
+            code:"RefreshToken_Already_Revoked",
+            description:"Token has already been revoked.");
+    }
+}
