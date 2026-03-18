2fbef478fb4b6b9943995fb6dbce822d34224a9c
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-03-19 01:34:35 +0300

    feat(domain) : add Roles as enum with values [Admin , Volunteer , TeamMember]

---
+++ Eghatha.Domain/Identity/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Domain.Identity
+{
+    public enum Role{
+        Admin , 
+        volunteer,
+        TeamMember
+    }
+}
