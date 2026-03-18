6b0e7caf9eacfb1c1fde58039b638178c31716ae
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-03-18 17:14:38 +0300

    feat(domain):add volunteerRegisteration class with RegisterationStatus and VolunteerRegisterationErrors.

---
+++ Eghatha.Domain/VolunteerRegisterations/VolunteerRegisteration.cs
@@ -0,0 +1,80 @@
+﻿using Eghatha.Domain.Abstractions;
+using Eghatha.Domain.Shared.Errors;
+using Eghatha.Domain.Volunteers;
+using ErrorOr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Domain.VolunteerRegisterations
+{
+    public sealed class VolunteerRegisteration : AuditableEntity
+    {
+        public Guid VolunteerId { get; private set; }
+
+        public RegisterationStatus Status { get; private set; }
+        public DateTimeOffset RequestedAt { get; private set; }
+
+        public DateTimeOffset? ReviewedAt { get; private set; }
+
+        public Guid? ReviewedByAdminId { get; private set; }
+
+        private VolunteerRegisteration()
+        {
+
+
+        }
+
+        private VolunteerRegisteration(
+            Guid id,
+            Guid volunteerId,
+            DateTimeOffset requestedAt
+                )
+            : base(id)
+        {
+            VolunteerId = volunteerId;
+            RequestedAt = requestedAt;
+            Status = RegisterationStatus.Pending;
+        }
+
+
+        public ErrorOr<VolunteerRegisteration> Create(Guid volunteerId, DateTimeOffset requestedAt)
+        {
+            if (volunteerId == Guid.Empty)
+            {
+                return DomainErrors.IdMustBeProvided(nameof(Volunteer));
+            }
+
+            return new VolunteerRegisteration(Guid.NewGuid(), volunteerId, requestedAt);
+        }
+
+
+        public ErrorOr<Updated> Approve(DateTimeOffset reviewedAt, Guid reviewedById)
+        {
+            if (Status != RegisterationStatus.Pending)
+                return VolunteerRegisterationErrors.AlreadyProcessed;
+
+            Status = RegisterationStatus.Approved;
+            ReviewedAt = reviewedAt;
+            ReviewedByAdminId = reviewedById;
+
+            return Result.Updated;
+        }
+
+        public ErrorOr<Updated> Reject(DateTimeOffset reviewedAt, Guid reviewedById)
+        {
+            if (Status != RegisterationStatus.Pending)
+                return VolunteerRegisterationErrors.AlreadyProcessed;
+
+            Status = RegisterationStatus.Rejected;
+            ReviewedAt = reviewedAt;
+            ReviewedByAdminId = reviewedById;
+
+
+            return Result.Updated;
+
+        }
+    }
+}
---
+++ Eghatha.Domain/VolunteerRegisterations/VolunteerRegisterationErrors.cs
@@ -0,0 +1,16 @@
+﻿using ErrorOr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Domain.VolunteerRegisterations
+{
+    public static class VolunteerRegisterationErrors
+    {
+        public static readonly Error AlreadyProcessed = Error.Conflict(
+            code: "VolunteerRegisterationErrors.AlreadyProcessed",
+            description: "registeration rqeuest already processed");
+    }
+}
