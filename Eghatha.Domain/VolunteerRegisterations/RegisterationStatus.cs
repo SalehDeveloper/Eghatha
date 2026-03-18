25b918cc109a5ed40d12673548facd53d8e6476e
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-03-18 17:13:36 +0300

    feat(domain):add volunteerRegisteration class with RegisterationStatus and VolunteerRegisterationErrors

---
+++ Eghatha.Domain/VolunteerRegisterations/RegisterationStatus.cs
@@ -0,0 +1,23 @@
+﻿using Ardalis.SmartEnum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Domain.VolunteerRegisterations
+{
+    public class RegisterationStatus : SmartEnum<RegisterationStatus>
+    {
+        public static readonly RegisterationStatus Pending = new RegisterationStatus(nameof(Pending), 1);
+
+        public static readonly RegisterationStatus Approved = new RegisterationStatus(nameof(Approved), 2);
+
+        public static readonly RegisterationStatus Rejected = new RegisterationStatus(nameof(Rejected), 3);
+
+
+        public RegisterationStatus(string name, int value) : base(name, value)
+        {
+        }
+    }
+}
