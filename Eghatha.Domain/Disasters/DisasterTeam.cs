0ae62c1d0875c1852c19c408e36a85a2cb58cf40
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-03-25 16:39:56 +0300

    feat(domain): Add junctiuon table for the realtion disaster-team

---
+++ Eghatha.Domain/Disasters/DisasterTeam.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Domain.Disasters
+{
+    public sealed class DisasterTeam
+    {
+        public Guid DisasterId { get; private set; }
+        public Guid TeamId { get; private set; }
+
+        private DisasterTeam() { }
+
+        public DisasterTeam(Guid disasterId, Guid teamId)
+        {
+            DisasterId = disasterId;
+            TeamId = teamId;
+        }
+    }
+}
