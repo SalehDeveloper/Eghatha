d592cb83f8b5869cad552a157f004513dbb8efa7
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-03-25 16:42:29 +0300

    feat(infrastructure): add applicationUser class to represent the user in the system

---
+++ Eghatha.Infastructure/Data/ApplicationUser.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Infastructure.Data
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; } 
+        public string PhotoUrl { get; private set; } 
+        public bool IsActive {  get; private set; }
+
+        public ApplicationUser(
+       string firstName,
+       string lastName,
+       string email,
+       string phoneNumber)
+        {
+            Id = Guid.NewGuid();
+
+            FirstName = firstName;
+            LastName = lastName;
+
+            Email = email;
+            UserName = email; 
+            PhoneNumber = phoneNumber;
+
+            IsActive = true;
+        }
+
+        public void Deactivate()
+        {
+            IsActive = false;
+        }
+
+        public void Activate()
+        {
+            IsActive = true;
+        }
+
+    }
+}
