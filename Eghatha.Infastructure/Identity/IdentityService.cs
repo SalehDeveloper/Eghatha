68bcbefb5b5389f5a6e7d009790559be35c916e5
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-03-28 18:30:10 +0300

    feat(Infrastructure) : add implemntation for IdentityService , CookieService , and JwtService

---
+++ Eghatha.Infastructure/Identity/IdentityService.cs
@@ -0,0 +1,59 @@
+﻿using Eghatha.Application.Common.Authentication;
+using Eghatha.Domain.Identity;
+using Eghatha.Infastructure.Data;
+using ErrorOr;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Infastructure.Identity
+{
+    public class IdentityService : IIdentityService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public IdentityService(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<ErrorOr<AppUserDto>> AuthenticateAsync(string email, string password , CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+
+            if (user == null)
+                return Error.NotFound("User_Not_Found", $"User with email {(email)} not found");
+
+            if (!user.EmailConfirmed)
+                return Error.Conflict("Email_Not_Confirmed", $"email '{(email)}' not confirmed");
+
+            if (!user.IsActive)
+                return Error.Conflict("User_Not_Actuve", $"user with '{(email)}' not active");
+
+            if ( await _userManager.CheckPasswordAsync(user , password) is false)
+                return Error.Conflict("Invalid_Login_Attempt", "Email / Password are incorrect");
+
+            return new AppUserDto(user.Id , user.Email , await _userManager.GetRolesAsync(user));
+
+
+
+
+
+        }
+
+        public async Task<ErrorOr<AppUserDto>> GetUserByIdAsync(Guid userId, CancellationToken cancellationToken)
+        {
+            var user =await _userManager.FindByIdAsync(userId.ToString());
+
+            if (user == null)
+                return Error.NotFound("User_Not_Found", $"User with id {(userId)} not found");
+            
+            var roles = await _userManager.GetRolesAsync(user);
+
+            return new AppUserDto(userId , user.Email , roles);
+        }
+    }
+}
---
+++ Eghatha.Infastructure/Identity/JwtService.cs
@@ -0,0 +1,71 @@
+﻿using Azure.Core;
+using Eghatha.Application.Common.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Infastructure.Identity
+{
+    public class JwtService : IJwtService
+    {
+      
+
+        private readonly AuthenticationOptions _options;
+        private readonly TimeProvider _timeProvider;
+        
+
+        public JwtService(IOptions<AuthenticationOptions> options, TimeProvider timeProvider)
+        {
+            _options = options.Value;
+            _timeProvider = timeProvider;
+           
+        }
+
+        public string GenerateAccessToken(AppUserDto user)
+        {
+            var claims = new List<Claim>
+            {
+                  new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
+                  new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                  new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+
+
+            };
+            foreach (var role in user.Roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.Key));
+
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+
+                issuer: _options.Issuer,
+                audience: _options.Audience,
+                claims: claims,
+                expires: _timeProvider.GetUtcNow().AddMinutes(_options.AccessTokenDurationInMinutes).UtcDateTime,
+                signingCredentials: signingCredentials
+                );
+
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public string GenerateRefreshToken()
+        {
+            return Convert.ToBase64String(RandomNumberGenerator.GetBytes(64));
+        }
+
+
+    }
+}
