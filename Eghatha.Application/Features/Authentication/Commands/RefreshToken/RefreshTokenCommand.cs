08edb8b72898b4206d821a05aea916941b90d5a3
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-03-28 18:32:33 +0300

    feat(Application) : Add RefreshToken Functionality with RefreshTokenCommand and RefreshTokenCommandHandler

---
+++ Eghatha.Application/Features/Authentication/Commands/RefreshToken/RefreshTokenCommand.cs
@@ -0,0 +1,12 @@
+﻿using ErrorOr;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Application.Features.Authentication.Commands.RefreshToken
+{
+    public record RefreshTokenCommand() : IRequest<ErrorOr<Success>>;
+}
