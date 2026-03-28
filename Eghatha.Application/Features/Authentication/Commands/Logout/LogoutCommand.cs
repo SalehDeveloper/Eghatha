7335fae384be3a8977a4dc41357ce037b4970e7b
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-03-28 18:33:10 +0300

    feat(Application) : Add Logout Functionality with LogoutCommand and LogoutCommandHandler

---
+++ Eghatha.Application/Features/Authentication/Commands/Logout/LogoutCommand.cs
@@ -0,0 +1,14 @@
+﻿using ErrorOr;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Application.Features.Authentication.Commands.Logout
+{
+    public record LogoutCommand : IRequest<ErrorOr<Success>>;
+    
+    
+}
