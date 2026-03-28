d3a633dcd2f11a6e6c1557cf49cf68fb251455cb
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-03-28 18:15:07 +0300

    feat(Application): Add ICookieService interface for handling cookies with SetAccessTokenInCookies , SetRefreshTokenInCookies , GetAccessToken , GetRefreshToken , DeleteAccessToken , DeleteRefreshToken methods

---
+++ Eghatha.Application/Common/Authentication/ICookieService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Application.Common.Authentication
+{
+    public interface  ICookieService
+    {
+        public void SetAccessTokenInCookies(string accessToken);
+        public void SetRefreshTokenInCookies(string refreshToken);
+
+       public string? GetAccessToken();
+       public string? GetRefreshToken();
+
+       public void DeleteAccessToken();
+        public void DeleteRefreshToken();
+
+    }
+}
