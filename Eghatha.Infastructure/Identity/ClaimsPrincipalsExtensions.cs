f924f44a7062ab435d1003e8459186bd520edcea
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-03-28 18:28:45 +0300

    Refactor(Infrastructure): Update ClaimsPrincipalsExtensions class

--- Eghatha.Infastructure/Identity/ClaimsPrincipalsExtensions.cs
+++ Eghatha.Infastructure/Identity/ClaimsPrincipalsExtensions.cs
@@ -19,17 +19,14 @@ namespace Eghatha.Infastructure.Identity
                 : throw new ApplicationException("User id is unavailable");
         }
 
-        public static Role GetUserRole(this ClaimsPrincipal? claims)
+        public static string GetUserRole(this ClaimsPrincipal claims)
         {
-            var roleValue = claims?.FindFirstValue(ClaimTypes.Role);
+            var role = claims.FindFirstValue(ClaimTypes.Role);
 
-            if (string.IsNullOrWhiteSpace(roleValue))
-                throw new ApplicationException("User role is unavailable.");
+            if (string.IsNullOrWhiteSpace(role))
+                throw new UnauthorizedAccessException("Role not found");
 
-            if (!Enum.TryParse<Role>(roleValue, ignoreCase: true, out var parsedRole))
-                throw new ApplicationException($"Invalid user role: {roleValue}");
-
-            return parsedRole;
+            return role;
         }
     }
 }
