14fb970bda4aa1ce7be0dedaf9e7a982128a6866
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-04-01 00:58:22 +0300

    feat(Api): Add Accounts , Dashboard and update identity controller

---
+++ Eghatha.Api/Controllers/DashboardsController.cs
@@ -0,0 +1,31 @@
+﻿using Eghatha.Application.Features.Dashboards.Queries.AccountStatistics;
+using Eghatha.Contract.Accounts.Responses;
+using Eghatha.Contract.Dashboards;
+using Eghatha.Contract.Shared;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eghatha.Api.Controllers
+{
+    public class DashboardsController : ApiController
+    {
+        public DashboardsController(ISender sender) : base(sender)
+        {
+        }
+
+
+
+        [HttpGet(ApiEndpoints.Dashboards.AccountStatistics)]
+        [ProducesResponseType(typeof(AccountStatisticsResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAccountStatistics(CancellationToken ct)
+        {
+            var result = await _sender.Send(new GetAccountStatisticsQuery(), ct);
+
+            return Ok(result);
+        }
+    }
+}
--- Eghatha.Api/Controllers/IdentityController.cs
+++ Eghatha.Api/Controllers/IdentityController.cs
@@ -1,6 +1,11 @@
 ﻿using Eghatha.Api.Mappers;
+using Eghatha.Application.Features.Authentication.Commands.ConfirmEmail;
 using Eghatha.Application.Features.Authentication.Commands.Login;
+using Eghatha.Application.Features.Authentication.Commands.Logout;
 using Eghatha.Application.Features.Authentication.Commands.RefreshToken;
+using Eghatha.Application.Features.Authentication.Commands.RequestPasswordReset;
+using Eghatha.Application.Features.Authentication.Commands.ResendEmailConfirmationCode;
+using Eghatha.Application.Features.Authentication.Commands.ResetPassword;
 using Eghatha.Application.Features.Authentication.Queries.GetLoggedInUser;
 using Eghatha.Contract.Identity.Requests;
 using Eghatha.Contract.Identity.Responses;
@@ -14,7 +19,7 @@ namespace Eghatha.Api.Controllers
     {
         public IdentityController(ISender sender) : base(sender)
         {
-     
+
         }
 
         [HttpPost(ApiEndpoints.Identity.Login)]
@@ -24,9 +29,9 @@ namespace Eghatha.Api.Controllers
         [EndpointSummary("Authenticate user and generate access , refresh tokens.")]
         [EndpointDescription("Validates the user's credentials (email and password) and returns an authenticated user response with a valid session if successful.")]
         [EndpointName("Login")]
-        public async Task<IActionResult> Login (LoginRequest request , CancellationToken cancellationToken)
+        public async Task<IActionResult> Login(LoginRequest request, CancellationToken cancellationToken)
         {
-            var command = new LoginCommand(request.Email, request.Password);    
+            var command = new LoginCommand(request.Email, request.Password);
 
             var result = await _sender.Send(command, cancellationToken);
 
@@ -38,7 +43,7 @@ namespace Eghatha.Api.Controllers
 
 
         [HttpPost(ApiEndpoints.Identity.RefreshToken)]
-        [ProducesResponseType( StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
         [EndpointSummary("Rotates authentication tokens using refresh token.")]
@@ -59,9 +64,10 @@ namespace Eghatha.Api.Controllers
         [HttpGet(ApiEndpoints.Identity.Me)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-        [ProducesResponseType(typeof(MeResponse) , StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(MeResponse), StatusCodes.Status200OK)]
         [EndpointSummary("Gets the current authenticated user profile.")]
         [EndpointDescription("Returns the profile information of the currently authenticated user")]
+        [EndpointName("Me")]
         public async Task<IActionResult> Me(CancellationToken cancellationToken)
         {
             var query = new GetLoggedinUserQuery();
@@ -71,17 +77,118 @@ namespace Eghatha.Api.Controllers
 
 
             return result.Match(
-                v=> base.Ok( v.ToMeResponse()),
+                v => base.Ok(v.ToMeResponse()),
                  Problem);
 
         }
 
+        [HttpPost(ApiEndpoints.Identity.RequestPasswordReset)]
+        [ProducesResponseType(typeof(RequestPasswordResetResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status409Conflict)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        [EndpointSummary("Initiates a password reset request.")]
+        [EndpointDescription("Generates and sends a password reset code to the user's email to allow resetting their password.")]
+        [EndpointName("RequestPasswordReset")]
+        public async Task<IActionResult> RequestPasswrodReset([FromBody] RequestPasswordResetRequest request, CancellationToken cancellationToken)
+        {
+            var command = new RequestPasswordResetCommand(request.Email);
+
+            var result = await _sender.Send(command, cancellationToken);
+
+            return result.Match(
+                 v => base.Ok(new RequestPasswordResetResponse(result.Value)),
+                 Problem);
+        }
+
+
+
+        [HttpPost(ApiEndpoints.Identity.ResetPassword)]
+        [ProducesResponseType(typeof(ResetPasswordResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        [EndpointSummary("Resets the user's password.")]
+        [EndpointDescription("Validates the provided reset code and updates the user's password to the new value.")]
+        [EndpointName("ResetPassword")]
+        public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordRequest request, CancellationToken cancellationToken)
+        {
+            var command = new ResetPasswordCommand(request.Email, request.Code, request.NewPassword);
+
+            var result = await _sender.Send(command, cancellationToken);
+
+            return result.Match(
+                 v => base.Ok(new ResetPasswordResponse(result.Value)),
+                 Problem);
+        }
+
+        [Authorize]
+        [HttpPost(ApiEndpoints.Identity.Logout)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        [EndpointSummary("Logs out the current user.")]
+        [EndpointDescription("Invalidates the current user's authentication token, effectively logging them out of the system.")]
+        [EndpointName("Logout")]
+        public async Task<IActionResult> Logout(CancellationToken cancellationToken)
+        {
+            var command = new LogoutCommand();
+            var result = await _sender.Send(command, cancellationToken);
+            return result.Match(
+                 v => base.NoContent(),
+                 Problem);
 
 
 
 
 
 
+
+
+        }
+
+
+        [HttpPost(ApiEndpoints.Identity.ConfirmEmail)]
+        [ProducesResponseType(typeof(ConfirmEmailResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status409Conflict)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        [EndpointSummary("Confirms user email using OTP code.")]
+        [EndpointDescription("Validates the provided OTP code sent to the user's email and confirm the user account if the code is correct.")]
+        [EndpointName("ConfirmEmail")]
+        public async Task<IActionResult> ConfirmEmail([FromBody] ConfirmEmailRequest request, CancellationToken cancellationToken)
+        {
+            var command = new ConfirmEmailCommand(request.Email, request.Otp);
+
+            var result = await _sender.Send(command, cancellationToken);
+
+            return result.Match(
+                 v => base.Ok(new ConfirmEmailResponse(result.Value)),
+                 Problem);
+        }
+
+
+        [HttpPost(ApiEndpoints.Identity.ResendEmailCode)]
+        [ProducesResponseType(typeof(ResendEmailCodeResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status409Conflict)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        [EndpointSummary("Resends email confirmation OTP code.")]
+        [EndpointDescription("Generates and sends a new OTP code to the user's email to allow email verification or re-verification.")]
+        [EndpointName("ResendEmailCode")]
+        public async Task<IActionResult> ResendEmailCode([FromBody] ResendEmailCodeRequest request, CancellationToken cancellationToken)
+        {
+            var command = new ResendEmailConfirmationCodeCommand(request.Email);
+
+            var result = await _sender.Send(command, cancellationToken);
+
+            return result.Match(
+                   v => base.Ok(new ResendEmailCodeResponse(result.Value)),
+                   Problem);
+        }
 
     }
 }
