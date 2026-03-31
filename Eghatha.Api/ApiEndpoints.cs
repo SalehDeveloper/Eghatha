7df352e7b6d9d212eab5f5bf1383dd3ebc25d367
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-04-01 00:59:09 +0300

    feat(Api): add required mappers and udpate ApiEndpoints class , adding Role to the api layer

--- Eghatha.Api/ApiEndpoints.cs
+++ Eghatha.Api/ApiEndpoints.cs
@@ -11,11 +11,38 @@ namespace Eghatha.Api
             public const string Base = $"{apiBase}/identity";
 
             public const string Login = $"{Base}/login";
+
             public const string RefreshToken = $"{Base}/refresh-token";
+
+            public const string Logout = $"{Base}/logout";
+
             public const string Me = $"{Base}/me";
 
+            public const string RequestPasswordReset = $"{Base}/request-password-reset";
+
+            public const string ResetPassword = $"{Base}/reset-password";
+
+            public const string ConfirmEmail = $"{Base}/confirm-email";
+
+            public const string ResendEmailCode = $"{Base}/resend-email-code";
 
 
+
+        }
+
+        public static class Accounts
+        {
+
+            public const string Base = $"{apiBase}/accounts";
+            public const string GetAll = $"{Base}";
+            public const string Activate = $"{Base}/{{id:guid}}/activate";
+            public const string DeActivate = $"{Base}/{{id:guid}}/deactivate";
+        }
+
+        public static class Dashboards
+        {
+            public const string Base = $"{apiBase}/dashboards";
+            public const string AccountStatistics = $"{Base}/account-statistics";
 
         }
     }
---
+++ Eghatha.Api/ApplicationRole.cs
@@ -0,0 +1,12 @@
+﻿namespace Eghatha.Api
+{
+    public class ApplicationRole
+    { 
+        public const string Admin = "Admin";
+
+        public const string TeamMember = "TeamMember";
+
+        public const string Volunteer = "Volunteer";
+
+    }
+}
