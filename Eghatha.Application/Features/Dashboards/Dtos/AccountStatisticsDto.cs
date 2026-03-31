4a05a1f3b188ea846174445afbecae4c49b1c0b3
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-04-01 00:57:47 +0300

    feat(Application) : add Dashboards Feature and updaing the dependencyinjection class

---
+++ Eghatha.Application/Features/Dashboards/Dtos/AccountStatisticsDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Application.Features.Dashboards.Dtos
+{
+    public record AccountStatisticsDto(int TotalUsers, int ActiveUsers, int InActiveUsers, int TotalAdmins);
+    
+    
+}
