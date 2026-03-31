734fcf3cf1b07117a2cc1afb9d17fdd7c7495818
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-04-01 00:43:31 +0300

    feat(contract) : add dashboards request and reposnes

---
+++ Eghatha.Contract/Dashboards/AccountStatisticsResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Contract.Dashboards
+{
+    public record AccountStatisticsResponse(int TotalUsers, int ActiveUsers, int InActiveUsers, int TotalAdmins);
+    
+}
