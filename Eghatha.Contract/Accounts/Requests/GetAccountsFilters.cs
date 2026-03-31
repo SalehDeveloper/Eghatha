1cc6a32a1a10d6998d205b685db3a96bb1ae4c71
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-04-01 00:42:40 +0300

    feat(Contract):add Account rqeuests and responses

---
+++ Eghatha.Contract/Accounts/Requests/GetAccountsFilters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Contract.Accounts.Requests
+{
+    public record GetAccountsFilters(
+       string? SearchTerm,
+       string? Role,
+        bool? IsActive
+    );
+    
+    
+}
