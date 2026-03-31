d4f3c0099a19a317f932bffb651690cdda310382
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-04-01 00:43:59 +0300

    feat(contract): add shared requests ,reposnses

---
+++ Eghatha.Contract/Shared/PagedRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Contract.Shared
+{
+    public record class PagedRequest(int Page =1 , int PageSize=10);
+    
+    
+}
