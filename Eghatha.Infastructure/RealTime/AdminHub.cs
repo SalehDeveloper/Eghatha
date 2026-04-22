e2efd8d8f9d100adc630499d4d10cf21916c2bed
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-04-22 09:22:11 +0300

    feat(infrastructure): add AdminHub , SignalRAdminNotifier   for real time tracking

---
+++ Eghatha.Infastructure/RealTime/AdminHub.cs
@@ -0,0 +1,24 @@
+﻿using Eghatha.Application.Common.Interfaces;
+using Eghatha.Domain.Identity;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Infastructure.RealTime
+{
+    public class AdminHub : Hub<IAdminTrackingClient>
+    {
+        public const string HubUrl = "/hubs/admin";
+
+        public override async Task OnConnectedAsync()
+        {
+            if (Context.User?.IsInRole("Admin") == true)
+                await Groups.AddToGroupAsync(Context.ConnectionId, "Admins");
+
+            await base.OnConnectedAsync();
+        }
+    }
+}
