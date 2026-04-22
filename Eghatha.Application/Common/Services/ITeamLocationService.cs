7597ccbf8d71733526c8ff45d2c7df93e807e96c
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-04-22 09:18:59 +0300

    feat(Application): Add IteamLocationservice

---
+++ Eghatha.Application/Common/Services/ITeamLocationService.cs
@@ -0,0 +1,17 @@
+﻿using Eghatha.Domain.Shared.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Application.Common.Services
+{
+    public interface ITeamLocationService
+    {
+        Task SetLocationAsync(Guid teamId, GeoLocation location);
+        Task<GeoLocation?> GetLocationAsync(Guid teamId);
+        Task<IReadOnlyList<(Guid teamId, double distance)>> GetNearbyTeamsAsync(
+            GeoLocation location, double radiusKm);
+    }
+}
