06609d72d534a79a55ddf74313c0826aa30df5d9
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-04-24 13:06:28 +0300

    feat(Application): Add ApproveRegisterationCommand with Handler and Validator

---
+++ Eghatha.Application/Features/VolunteerRegisterations/Commands/ApproveRegisteration/ApproveRegisterationCommand.cs
@@ -0,0 +1,14 @@
+﻿using Eghatha.Application.Features.VolunteerRegisterations.Commands.RejectRegisteration;
+using ErrorOr;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Application.Features.VolunteerRegisterations.Commands.ApproveRegisteration
+{
+    public record ApproveRegisterationCommand(Guid RegisterationId) : IRequest<ErrorOr<Updated>>;
+
+}
