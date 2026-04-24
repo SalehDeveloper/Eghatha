92da239404b81d28123ff2ab48c7f07db8ba0bc3
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-04-24 13:09:36 +0300

    feat(Application): Add GetAllRegisterations Query with queryHandler and Dto

---
+++ Eghatha.Application/Features/VolunteerRegisterations/Dtos/VolunteerRegisterationDto.cs
@@ -0,0 +1,28 @@
+﻿using Eghatha.Domain.Shared.ValueObjects;
+using Eghatha.Domain.VolunteerRegisterations;
+using Eghatha.Domain.Volunteers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Application.Features.VolunteerRegisterations.Dtos
+{
+    public record VolunteerRegisterationDto(Guid Id,
+    Guid VolunteerId,
+    string FullName,
+    string Email,
+    string PhoneNumber ,
+    string Photo,
+    double Latitude,
+    double Longitude,
+    int YearsOfExperince , 
+    VolunteerSpeciality Speciality ,
+    string Cv ,
+    RegisterationStatus Status,
+    DateTimeOffset RequestedAt,
+    DateTimeOffset? ReviewedAt,
+    string? RejectionReason);
+    
+}
---
+++ Eghatha.Application/Features/VolunteerRegisterations/Queries/GetAll/GetAllRegisterationsQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Eghatha.Application.Common.Interfaces;
+using Eghatha.Application.Common.Models;
+using Eghatha.Application.Features.VolunteerRegisterations.Dtos;
+using MediatR;
+
+namespace Eghatha.Application.Features.VolunteerRegisterations.Queries.GetAll
+{
+    public class GetAllRegisterationsQueryHandler : IRequestHandler<GetAllRegisterationsQuery, PaginatedList<VolunteerRegisterationDto>>
+    {
+        private readonly IVolunteerRegisterationRepository _repository;
+
+        public GetAllRegisterationsQueryHandler(IVolunteerRegisterationRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<PaginatedList<VolunteerRegisterationDto>> Handle(GetAllRegisterationsQuery request, CancellationToken cancellationToken)
+        {
+            return await _repository.GetRegisterationsAsync(request.Page, request.PageSize, request.SearchTerm, request.Status, cancellationToken);
+        }
+    }
+}
