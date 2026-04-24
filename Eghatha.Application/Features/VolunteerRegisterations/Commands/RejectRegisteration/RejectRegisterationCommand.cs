575e464f25ad1814cb5dd125cdf2f23d94d47e84
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-04-24 13:06:49 +0300

    feat(Application): Add RejectRegisterationCommand with Handler and Validator

---
+++ Eghatha.Application/Features/VolunteerRegisterations/Commands/RejectRegisteration/RejectRegisterationCommand.cs
@@ -0,0 +1,12 @@
+﻿using ErrorOr;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Application.Features.VolunteerRegisterations.Commands.RejectRegisteration
+{
+    public record RejectRegisterationCommand(Guid RegisterationId , string Reason) : IRequest<ErrorOr<Updated>>;
+}
