fcc2fe5c7205ea656d31cc4800a66fa151d5849e
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-04-24 12:49:22 +0300

    feat(domain): add VolunteerRegisteration Events

---
+++ Eghatha.Domain/VolunteerRegisterations/Events/VolunteerRegisterationRejected.cs
@@ -0,0 +1,17 @@
+﻿using Eghatha.Domain.Abstractions;
+
+namespace Eghatha.Domain.VolunteerRegisterations.Events
+{
+    public sealed class VolunteerRegisterationRejected : DomainEvent
+    {
+        public VolunteerRegisterationRejected(Guid volunteerId, string reason)
+        {
+            VolunteerId = volunteerId;
+            Reason = reason;
+        }
+
+        public Guid VolunteerId { get; set; }
+
+        public string Reason { get; set; }  
+    }
+}
