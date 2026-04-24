81de25cc0ff609e6ecbb79df0b43c2c4ca0f6483
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-04-24 13:10:24 +0300

    feat(Contract): Add requried requests and reponses for VolunteerRegisteration Entity

---
+++ Eghatha.Contract/VolunteerRegisterations/Responses/VolunteerRegisterationResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Contract.VolunteerRegisterations.Responses
+{
+    public record VolunteerRegisterationResponse(Guid Id,
+    Guid VolunteerId,
+    string FullName,
+    string Email,
+    string PhoneNumber,
+    string Photo,
+    double Latitude,
+    double Longitude,
+    int YearsOfExperince,
+    string Speciality,
+    string Cv,
+    string Status,
+    DateTimeOffset RequestedAt,
+    DateTimeOffset? ReviewedAt,
+    string? RejectionReason
+        );
+    
+    
+}
