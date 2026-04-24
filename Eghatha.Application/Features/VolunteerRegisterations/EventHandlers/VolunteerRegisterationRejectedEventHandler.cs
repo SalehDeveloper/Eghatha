41562d004c5b2749bdc50bb1778f9535cd1ad1b8
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-04-24 13:07:34 +0300

    feat(Application): Add Event Handlers for VolunteerRegisteration entity

---
+++ Eghatha.Application/Features/VolunteerRegisterations/EventHandlers/VolunteerRegisterationRejectedEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using Eghatha.Application.Common.Authentication;
+using Eghatha.Application.Common.Interfaces;
+using Eghatha.Application.Common.Services;
+using Eghatha.Domain.VolunteerRegisterations.Events;
+using MediatR;
+
+namespace Eghatha.Application.Features.VolunteerRegisterations.EventHandlers
+{
+    public class VolunteerRegisterationRejectedEventHandler : INotificationHandler<VolunteerRegisterationRejected>
+    {
+        private readonly IIdentityService _identityService;
+        private readonly IEmailService _emailService;
+        private readonly IVolunteerRepository _volunteerRepository;
+
+        public VolunteerRegisterationRejectedEventHandler(IIdentityService identityService, IEmailService emailService, IVolunteerRepository volunteerRepository)
+        {
+            _identityService = identityService;
+            _emailService = emailService;
+            _volunteerRepository = volunteerRepository;
+        }
+
+        public async Task Handle(VolunteerRegisterationRejected notification, CancellationToken cancellationToken)
+        {
+            var volunteer = await _volunteerRepository.GetByIdAsync(notification.VolunteerId, cancellationToken);
+            var user = await _identityService.GetUserDetailsByIdAsync(volunteer.UserId, cancellationToken);
+
+            await _emailService.SendVolunteerRejectedEmailAsync(user.Value.Email, $"{user.Value.FirstName} {user.Value.LastName}" , notification.Reason);
+        }
+    }
+}
