9f27c1fd740b99b48efc9170fd28ade9ed386f32
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-05-04 01:23:03 +0300

    feat(Application): add GetVolunteerEquipments query with handler and validator

---
+++ Eghatha.Application/Features/Volunteers/Queries/GetEquipments/GetVolunteerEquipmentsQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using Eghatha.Application.Common.Interfaces;
+using Eghatha.Application.Common.Models;
+using Eghatha.Application.Features.Volunteers.Dtos;
+using MediatR;
+
+namespace Eghatha.Application.Features.Volunteers.Queries.GetEquipments
+{
+    public class GetVolunteerEquipmentsQueryHandler
+    : IRequestHandler<GetVolunteerEquipmentsQuery, PaginatedList<VolunteerEquipmentDto>>
+    {
+        private readonly IVolunteerRepository _repo;
+
+        public GetVolunteerEquipmentsQueryHandler(
+            IVolunteerRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<PaginatedList<VolunteerEquipmentDto>> Handle(
+            GetVolunteerEquipmentsQuery request,
+            CancellationToken cancellationToken)
+        {
+            return await _repo.GetVolunteerEquipmentsAsync(
+                request.VolunteerId,
+                request.Page,
+                request.PageSize,
+                request.Category,
+                cancellationToken);
+        }
+    }
+
+}
