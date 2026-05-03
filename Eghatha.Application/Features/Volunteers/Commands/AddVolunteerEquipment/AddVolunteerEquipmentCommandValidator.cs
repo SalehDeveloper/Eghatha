d55bef5235ef68cef5fabbbeb5c09c1d1e777b8f
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-05-04 01:06:48 +0300

    feat(Application): Add  AddVolunteerEquipmentCommand with handler and validator

---
+++ Eghatha.Application/Features/Volunteers/Commands/AddVolunteerEquipment/AddVolunteerEquipmentCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+
+namespace Eghatha.Application.Features.Volunteers.Commands.AddVolunteerEquipment
+{
+    public sealed class AddVolunteerEquipmentCommandValidator
+    : AbstractValidator<AddVolunteerEquipmentCommand>
+    {
+        public AddVolunteerEquipmentCommandValidator()
+        {
+            RuleFor(x => x.VolunteerId)
+                .NotEmpty()
+                .WithMessage("VolunteerId is required.");
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Equipment name is required.")
+                .MaximumLength(100)
+                .WithMessage("Equipment name must not exceed 100 characters.");
+
+          
+
+            RuleFor(x => x.Quantity)
+                .GreaterThan(0)
+                .WithMessage("Quantity must be greater than 0.");
+        }
+    }
+}
