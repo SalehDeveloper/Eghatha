0be684ae1796d405457ea183f26850a5b16a4da5
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-05-04 01:17:30 +0300

    feat(Application): Add UpdateVolunteerStatus command with handler and validator

---
+++ Eghatha.Application/Features/Volunteers/Commands/UpdateStatus/UpdateVolunteerStatusCommand.cs
@@ -0,0 +1,17 @@
+﻿using Eghatha.Domain.Shared.Errors;
+using Eghatha.Domain.Volunteers;
+using ErrorOr;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Application.Features.Volunteers.Commands.UpdateStatus
+{
+    public record UpdateVolunteerStatusCommand(
+     Guid VolunteerId,
+     VolunteerStatus Status
+ ) : IRequest<ErrorOr<Updated>>;
+}
---
+++ Eghatha.Application/Features/Volunteers/Commands/UpdateStatus/UpdateVolunteerStatusCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using Eghatha.Application.Common.Errors;
+using Eghatha.Application.Common.Interfaces;
+using Eghatha.Domain.Abstractions;
+using ErrorOr;
+using MediatR;
+using Microsoft.Extensions.Caching.Hybrid;
+
+namespace Eghatha.Application.Features.Volunteers.Commands.UpdateStatus
+{
+    public class UpdateVolunteerStatusCommandHandler
+    : IRequestHandler<UpdateVolunteerStatusCommand, ErrorOr<Updated>>
+    {
+        private readonly IVolunteerRepository _repository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly HybridCache _hybridCache;
+
+
+        public UpdateVolunteerStatusCommandHandler(IVolunteerRepository repository, IUnitOfWork unitOfWork, HybridCache hybridCache)
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+            _hybridCache = hybridCache;
+        }
+
+        public async Task<ErrorOr<Updated>> Handle(
+            UpdateVolunteerStatusCommand request,
+            CancellationToken cancellationToken)
+        {
+            var volunteer = await _repository.GetByIdAsync(request.VolunteerId, cancellationToken);
+
+            if (volunteer is null)
+                return ApplicationErrors.VolunteerNotFound;
+
+            var result = volunteer.UpdateStatus(request.Status);
+
+            if (result.IsError)
+                return result.Errors;
+
+            await _unitOfWork.CompleteAsync(cancellationToken);
+            await _hybridCache.RemoveByTagAsync("volunteers");
+            return Result.Updated;
+        }
+    }
+}
