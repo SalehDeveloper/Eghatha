208b849480872b19a6e49e973e3f64d2e55aa7e4
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-05-04 01:10:19 +0300

    feat(Application): Add RemoveEquipmentCommand with handler and validator

---
+++ Eghatha.Application/Features/Volunteers/Commands/RemoveVolunteerEquipment/RemoveVolunteerEquipmentCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace Eghatha.Application.Features.Volunteers.Commands.RemoveVolunteerEquipment
+{
+    public sealed class RemoveVolunteerEquipmentCommandValidator
+    : AbstractValidator<RemoveVolunteerEquipmentCommand>
+    {
+        public RemoveVolunteerEquipmentCommandValidator()
+        {
+            RuleFor(x => x.VolunteerId)
+                .NotEmpty()
+                .WithMessage("VolunteerId is required.");
+
+            RuleFor(x => x.EquipmentId)
+                .NotEmpty()
+                .WithMessage("EquipmentId is required.");
+        }
+    }
+}
