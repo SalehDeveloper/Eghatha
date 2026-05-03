a95e59b598c86d01d84fcc4857a1e9f6e3be23cf
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-05-04 00:58:46 +0300

    feat(Application): Add IncreaseTeamRsourceCommandValidator DecreaseTeamRsourceCommandValidator

---
+++ Eghatha.Application/Features/Teams/Commands/IncreaseTeamResource/IncreaseTeamResourceCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Eghatha.Application.Features.Teams.Commands.IncreaseTeamResource
+{
+    public class IncreaseTeamResourceCommandValidator
+    : AbstractValidator<IncreaseTeamResourceCommand>
+    {
+        public IncreaseTeamResourceCommandValidator()
+        {
+            RuleFor(x => x.TeamId)
+                .NotEmpty().WithMessage("TeamId is required");
+
+            RuleFor(x => x.ResourceId)
+                .NotEmpty().WithMessage("ResourceId is required");
+
+            RuleFor(x => x.Quantity)
+                .GreaterThan(0)
+                .WithMessage("Quantity must be greater than 0")
+                .LessThanOrEqualTo(100)
+                .WithMessage("Quantity is too large");
+        }
+    }
+}
