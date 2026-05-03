848bb09ca9ecdd6392d7e19107408a09e68752ba
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-05-04 01:04:29 +0300

    feat(Application): Add VolunteerDto , VolunteerEquipmentDto

---
+++ Eghatha.Application/Features/Volunteers/Dtos/VolunteerDto.cs
@@ -0,0 +1,21 @@
+﻿using Eghatha.Domain.Volunteers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Application.Features.Volunteers.Dtos
+{
+    public record VolunteerDto(Guid Id,
+        string FullName,
+        string Email,
+        string PhoneNumber,
+        VolunteerStatus Status,
+        VolunteerSpeciality Speciality,
+        string province,
+        string city,
+        int YearsOfExperience,
+        double AverageScore);
+
+}
