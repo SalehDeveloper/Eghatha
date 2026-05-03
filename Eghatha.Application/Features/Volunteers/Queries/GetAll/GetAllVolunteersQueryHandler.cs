4438189e831795dc76b3eac7292952713fc80654
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-05-04 01:20:54 +0300

    feat(Application): add GetAllVolunteers query with handler and validator

---
+++ Eghatha.Application/Features/Volunteers/Queries/GetAll/GetAllVolunteersQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Eghatha.Application.Common.Interfaces;
+using Eghatha.Application.Common.Models;
+using Eghatha.Application.Features.Volunteers.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Application.Features.Volunteers.Queries.GetAll
+{
+    public class GetAllVolunteersQueryHandler : IRequestHandler<GetAllVolunteersQuery, PaginatedList<VolunteerDto>>
+    {
+        private readonly IVolunteerRepository _repository;
+
+        public GetAllVolunteersQueryHandler(IVolunteerRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<PaginatedList<VolunteerDto>> Handle(GetAllVolunteersQuery request, CancellationToken cancellationToken)
+        {
+            return await _repository.GetVolunteersAsync(request.Page, request.PageSize, request.SearchTerm, request.Status, request.Speciality, request.Province, request.City , cancellationToken);
+        }
+    }
+}
---
+++ Eghatha.Application/Features/Volunteers/Queries/GetAll/GetAllVolunteersQueryValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+
+namespace Eghatha.Application.Features.Volunteers.Queries.GetAll
+{
+    public sealed class GetAllVolunteersQueryValidator
+    : AbstractValidator<GetAllVolunteersQuery>
+    {
+        public GetAllVolunteersQueryValidator()
+        {
+            
+            When(x => !string.IsNullOrWhiteSpace(x.SearchTerm), () =>
+            {
+                RuleFor(x => x.SearchTerm)
+                    .MaximumLength(100)
+                    .WithMessage("SearchTerm must not exceed 100 characters.");
+            });
+
+          
+
+            When(x => !string.IsNullOrWhiteSpace(x.Province), () =>
+            {
+                RuleFor(x => x.Province)
+                    .MaximumLength(100)
+                    .WithMessage("Province must not exceed 100 characters.");
+            });
+
+            When(x => !string.IsNullOrWhiteSpace(x.City), () =>
+            {
+                RuleFor(x => x.City)
+                    .MaximumLength(100)
+                    .WithMessage("City must not exceed 100 characters.");
+            });
+        }
+    }
+}
