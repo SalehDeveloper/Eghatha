48fa02693727b3ec99a3885c06415c93a14a2966
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-05-04 01:09:32 +0300

    feat(Application): IncreaseVolunteerEquipmentQuantity command with handler and validator

---
+++ Eghatha.Application/Features/Volunteers/Commands/IncreaseVolunteerEquipment/IncreaseVolunteerEquipmentQuantityCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using Eghatha.Application.Common.Errors;
+using Eghatha.Application.Common.Interfaces;
+using Eghatha.Domain.Abstractions;
+using ErrorOr;
+using MediatR;
+using Microsoft.Extensions.Caching.Hybrid;
+
+namespace Eghatha.Application.Features.Volunteers.Commands.IncreaseVolunteerEquipment
+{
+    public sealed class IncreaseVolunteerEquipmentQuantityCommandHandler
+    : IRequestHandler<IncreaseVolunteerEquipmentQuantityCommand, ErrorOr<Updated>>
+    {
+        private readonly IVolunteerRepository _repo;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly HybridCache _cache;
+
+        public IncreaseVolunteerEquipmentQuantityCommandHandler(
+            IVolunteerRepository repo,
+            IUnitOfWork unitOfWork,
+            HybridCache cache)
+        {
+            _repo = repo;
+            _unitOfWork = unitOfWork;
+            _cache = cache;
+        }
+
+        public async Task<ErrorOr<Updated>> Handle(
+            IncreaseVolunteerEquipmentQuantityCommand request,
+            CancellationToken cancellationToken)
+        {
+            var volunteer = await _repo.GetByIdWithEquipmentsAsync(request.VolunteerId, cancellationToken);
+
+            if (volunteer is null)
+                return ApplicationErrors.VolunteerNotFound;
+
+            var result = volunteer.IncreaseEquipmentQuantity(request.EquipmentId, request.Quantity);
+
+            if (result.IsError)
+                return result.Errors;
+
+            await _unitOfWork.CompleteAsync(cancellationToken);
+
+            await _cache.RemoveByTagAsync("volunteers");
+
+            return Result.Updated;
+        }
+    }
+}
