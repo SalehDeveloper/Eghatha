07c02f8c7acd88360689c8e71a60c39514e1bd90
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-05-04 01:21:37 +0300

    feat(Application): add GetVolunteerById query with handler and validator

---
+++ Eghatha.Application/Features/Volunteers/Queries/GetById/GetVolunteerByIdQueryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Eghatha.Application.Features.Volunteers.Queries.GetById
+{
+    public sealed class GetVolunteerByIdQueryValidator
+    : AbstractValidator<GetVolunteerByIdQuery>
+    {
+        public GetVolunteerByIdQueryValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .WithMessage("Volunteer Id is required.");
+        }
+    }
+}
