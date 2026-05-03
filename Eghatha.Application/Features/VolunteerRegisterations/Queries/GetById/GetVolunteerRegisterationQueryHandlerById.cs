11e7821969f081d41b19c9e397114ade7a125a73
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-05-04 01:03:36 +0300

    feat(Application): Add GetVolunteerRegisterationById Query with handler

---
+++ Eghatha.Application/Features/VolunteerRegisterations/Queries/GetById/GetVolunteerRegisterationQueryHandlerById.cs
@@ -0,0 +1,25 @@
+﻿using Eghatha.Application.Common.Errors;
+using Eghatha.Application.Common.Interfaces;
+using Eghatha.Application.Features.VolunteerRegisterations.Dtos;
+using ErrorOr;
+using MediatR;
+
+namespace Eghatha.Application.Features.VolunteerRegisterations.Queries.GetById
+{
+    public class GetVolunteerRegisterationQueryHandlerById : IRequestHandler<GetVolunteerRegisterationQueryById, ErrorOr<VolunteerRegisterationDto>>
+    {
+        private readonly IVolunteerRegisterationRepository _volunteerRegisterationRepository;
+
+        public GetVolunteerRegisterationQueryHandlerById(IVolunteerRegisterationRepository volunteerRegisterationRepository)
+        {
+            _volunteerRegisterationRepository = volunteerRegisterationRepository;
+        }
+
+        public async Task<ErrorOr<VolunteerRegisterationDto>> Handle(GetVolunteerRegisterationQueryById request, CancellationToken cancellationToken)
+        {
+            var res = await _volunteerRegisterationRepository.GetRegisterationByIdAsync(request.RegisterationId, cancellationToken);
+
+            return res is not null ? res : ApplicationErrors.RegisterationNotFound;
+        }
+    }
+}
