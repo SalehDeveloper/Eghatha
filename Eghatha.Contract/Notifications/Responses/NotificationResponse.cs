74a514f8929ea07688189b50edbec03f6faec1a8
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-05-24 18:26:07 +0300

    feat(Contract) : Add NotificationResponse

---
+++ Eghatha.Contract/Notifications/Responses/NotificationResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Contract.Notifications.Responses
+{
+    public sealed record NotificationResponse(Guid Id, string Title, string Message, string Url, string Type, bool IsRead, DateTimeOffset CreatedAt);
+    
+    
+}
