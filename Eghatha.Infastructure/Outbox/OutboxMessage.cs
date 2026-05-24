8c8408c86a173d01fed899d68632e14238ae4582
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-05-24 16:54:47 +0300

    feat(infra): Add OutboxMessage class with options

---
+++ Eghatha.Infastructure/Outbox/OutboxMessage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Infastructure.Outbox
+{
+    public sealed class OutboxMessage
+    {
+        public OutboxMessage(Guid id, string type, string content, DateTime occurredOnUtc)
+        {
+            Id = id;
+            Type = type;
+            Content = content;
+            OccurredOnUtc = occurredOnUtc;
+        }
+
+        public Guid Id { get; init; }
+
+        public string Type { get; init; }
+
+        public string Content { get; init; }
+
+        public DateTime OccurredOnUtc { get; init; }
+
+        public DateTime? ProcessedOnUtc { get; set; }
+
+        public string? Error { get; set; }
+    }
+}
