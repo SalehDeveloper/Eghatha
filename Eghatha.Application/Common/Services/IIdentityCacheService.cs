3c0cc9e89232dc747538bf1d7ab074050a96b0ce
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-05-24 16:53:57 +0300

    feat(App,infra): Add IIdentityCacheService with implmemnation

---
+++ Eghatha.Application/Common/Services/IIdentityCacheService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Application.Common.Services
+{
+    public interface  IIdentityCacheService
+    {
+        Task<List<Guid>> GetAdminIdsAsync(CancellationToken cancellationToken);
+
+        Task InvalidateAdminsAsync();
+    }
+}
---
+++ Eghatha.Infastructure/Services/IdentityCacheService.cs
@@ -0,0 +1,39 @@
+﻿using Eghatha.Application.Common.Authentication;
+using Eghatha.Application.Common.Services;
+using Microsoft.Extensions.Caching.Hybrid;
+
+namespace Eghatha.Infastructure.Services
+{
+    public class IdentityCacheService : IIdentityCacheService
+    {
+        private const string AdminKey = "identity:admins";
+
+        private readonly HybridCache _cache;
+        private readonly IIdentityService _identityService;
+
+        public IdentityCacheService(
+            HybridCache cache,
+            IIdentityService identityService)
+        {
+            _cache = cache;
+            _identityService = identityService;
+        }
+
+        public async Task<List<Guid>> GetAdminIdsAsync(CancellationToken cancellationToken)
+        {
+            return await _cache.GetOrCreateAsync(
+                AdminKey,
+                async _ => await _identityService.GetAdminIdsAsync(cancellationToken),
+                new HybridCacheEntryOptions
+                {
+                    Expiration = TimeSpan.FromMinutes(60)
+                },
+                cancellationToken: cancellationToken);
+        }
+
+        public async Task InvalidateAdminsAsync()
+        {
+            await _cache.RemoveAsync(AdminKey);
+        }
+    }
+}
