91f9a085ecc37f5957c9c5e6df8ef0c8da3849fc
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-05-24 18:24:40 +0300

    feat(App) : Add GetById Query with QueryHandler

---
+++ Eghatha.Application/Features/Notifications/Queries/GetById/GetNotificationByIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Eghatha.Application.Common.Errors;
+using Eghatha.Application.Common.Interfaces;
+using Eghatha.Application.Features.Notifications.Dtos;
+using ErrorOr;
+using MediatR;
+
+namespace Eghatha.Application.Features.Notifications.Queries.GetById
+{
+    public sealed class GetNotificationByIdQueryHandler : IRequestHandler<GetNotificationByIdQuery, ErrorOr<NotificationDto>>
+    {
+        private readonly INotificationRepository _notificationRepository;
+        private readonly IUser _user;
+
+        public GetNotificationByIdQueryHandler(INotificationRepository notificationRepository, IUser user)
+        {
+            _notificationRepository = notificationRepository;
+            _user = user;
+        }
+
+        public async Task<ErrorOr<NotificationDto>> Handle(GetNotificationByIdQuery request, CancellationToken cancellationToken)
+        {
+            var userId = _user.Id.Value;
+            
+            var notification = await _notificationRepository.GetNotificationByIdAsync(request.NotificationId, userId, cancellationToken);
+
+            if (notification is null) return ApplicationErrors.NotificationNotFound;
+
+            return notification;
+        }
+    }
+}
