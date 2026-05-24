60cd695216a3744427a8bfd5712ea09bf4887f8e
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-05-24 18:25:17 +0300

    feat(App): Add GetUnreadCountQuery with handler

---
+++ Eghatha.Application/Features/Notifications/Queries/GetUnreadCount/GetUnreadCountQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Eghatha.Application.Common.Interfaces;
+using MediatR;
+
+namespace Eghatha.Application.Features.Notifications.Queries.GetUnreadCount
+{
+    public sealed class GetUnreadCountQueryHandler : IRequestHandler<GetUnreadCountQuery, int>
+    {
+        private readonly INotificationRepository _notificationRepository;
+        private readonly IUser _user;
+
+        public GetUnreadCountQueryHandler(INotificationRepository notificationRepository, IUser user)
+        {
+            _notificationRepository = notificationRepository;
+            _user = user;
+        }
+        public async Task<int> Handle(GetUnreadCountQuery request, CancellationToken cancellationToken)
+        {
+            var userId = _user.Id.Value;
+            return await _notificationRepository.GetUnreadCountAsync( userId, cancellationToken);
+        }
+    }
+}
