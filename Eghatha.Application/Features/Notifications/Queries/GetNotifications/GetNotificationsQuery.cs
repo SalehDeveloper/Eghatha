1d092d28b3420c386d024bc18885d7eb96c96abd
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-05-24 18:25:02 +0300

    feat(App): Add GetAllNotificationsQuery with handler

---
+++ Eghatha.Application/Features/Notifications/Queries/GetNotifications/GetNotificationsQuery.cs
@@ -0,0 +1,15 @@
+﻿using Eghatha.Application.Common.Models;
+using Eghatha.Application.Features.Notifications.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Application.Features.Notifications.Queries.GetNotifications
+{
+    public sealed record GetNotificationsQuery(int Page, int PageSize) : IRequest<PaginatedList<NotificationDto>>;
+
+
+}
---
+++ Eghatha.Application/Features/Notifications/Queries/GetNotifications/GetNotificationsQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Eghatha.Application.Common.Interfaces;
+using Eghatha.Application.Common.Models;
+using Eghatha.Application.Features.Notifications.Dtos;
+using MediatR;
+
+namespace Eghatha.Application.Features.Notifications.Queries.GetNotifications
+{
+    public sealed class GetNotificationsQueryHandler : IRequestHandler<GetNotificationsQuery, PaginatedList<NotificationDto>>
+    {
+        private readonly INotificationRepository _notificationRepository;
+        private readonly IUser _user;
+
+        public GetNotificationsQueryHandler(INotificationRepository notificationRepository, IUser user)
+        {
+            _notificationRepository = notificationRepository;
+            _user = user;
+        }
+        public async Task<PaginatedList<NotificationDto>> Handle(GetNotificationsQuery request, CancellationToken cancellationToken)
+        {
+            var userId = _user.Id;
+            var notifications = await _notificationRepository.GetNotificationsByUserIdAsync(userId.Value , request.Page, request.PageSize, cancellationToken);
+
+
+            return notifications;
+        }
+    }
+
+
+}
