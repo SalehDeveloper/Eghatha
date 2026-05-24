2f12ef44524bdc7b0c7f3bfef8d61e8bc79a1f02
Ahmad Saleh <ahmadsalehdev.22@gmail.com>
2026-05-24 16:45:08 +0300

    feat(Domain) : Add Notification domain entity with Errors and Types

---
+++ Eghatha.Domain/Notifications/NotificationErrors.cs
@@ -0,0 +1,30 @@
+﻿using ErrorOr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Domain.Notifications
+{
+    public static class NotificationErrors
+    { 
+        public static readonly Error InvalidTitle = Error.Validation(
+            code:"Notification.InvalidTitle",
+            description:"The notification title is invalid.");
+
+
+        public static readonly Error InvalidMessage = Error.Validation(
+            code: "Notification.InvalidMessage",
+            description: "The notification message is invalid.");
+
+
+        public static readonly Error InvalidUrl = Error.Validation(
+            code: "Notification.InvalidUrl",
+            description: "The notification url is invalid.");
+
+
+
+
+    }
+}
