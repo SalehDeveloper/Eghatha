05ff584d53a5d93e2097ef82c7581c2e2d0680fc
SalehDeveloper <ahmadsalehdev.22@gmail.com>
2026-08-30 03:58:49 +0300

    feat(Application) : Add Logging Behavior

---
+++ Eghatha.Application/Common/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,37 @@
+﻿using Eghatha.Application.Common.Authentication;
+using Eghatha.Application.Common.Interfaces;
+using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eghatha.Application.Common.Behaviors
+{
+    public class LoggingBehavior<TRequest>(ILogger<TRequest> logger, IUser user, IIdentityService identityService)
+    : IRequestPreProcessor<TRequest>
+    where TRequest : notnull
+    {
+        private readonly ILogger _logger = logger;
+        private readonly IUser _user = user;
+        private readonly IIdentityService _identityService = identityService;
+
+        public async Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            var requestName = typeof(TRequest).Name;
+            var userId = _user.Id ?? Guid.Empty;
+            string? userName = string.Empty;
+
+            if (userId != Guid.Empty)
+            {
+                userName = await _identityService.GetUserNameAsync(userId);
+            }
+
+            _logger.LogInformation(
+                "Request: {Name} {@UserId} {@UserName} {@Request}", requestName, userId, userName, request);
+        }
+
+    }
+}
