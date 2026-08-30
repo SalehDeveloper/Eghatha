8eac48cb16f8afdca313d5c2438fcfcef108f70e
Ayham Alaama <dotnet_dev@mail.ru>
2026-08-30 19:23:42 +0300

    set base logic of localization

--- Eghatha.Api/Program.cs
+++ Eghatha.Api/Program.cs
@@ -1,24 +1,12 @@
-using Bogus;
 using Eghatha.Api;
 using Eghatha.Application;
-using Eghatha.Application.Common.Interfaces;
-using Eghatha.Domain.Shared.ValueObjects;
-using Eghatha.Domain.Disasters;
-using System.Linq;
-using Eghatha.Domain.Teams;
 using Eghatha.Domain.Teams.Resources;
-using Eghatha.Domain.Teams.TeamMembers;
-using Eghatha.Domain.VolunteerRegisterations;
-using Eghatha.Domain.Volunteers;
-using Eghatha.Domain.Volunteers.Equipments;
 using Eghatha.Infastructure;
-using Eghatha.Infastructure.Data;
 using Eghatha.Infastructure.RealTime.Admin;
 using Eghatha.Infastructure.RealTime.Team;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+using FluentValidation;
 using Scalar.AspNetCore;
-using StackExchange.Redis;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -38,11 +26,11 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://127.0.0.1:5500" , "http://localhost:5500" , "https://localhost:7243")
+                          policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500", "https://localhost:7243")
                                 .AllowAnyHeader()
                                 .AllowAnyMethod()
                                 .AllowCredentials();
-                                
+
                       });
 
 
@@ -89,7 +77,19 @@ else
     app.UseHsts();
 }
 
+//TODO: Add middleware to handle selected language from request header
 
+var language = "ar";
+
+CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo(language);
+
+Thread.CurrentThread.CurrentCulture = new CultureInfo(language);
+
+Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
+
+ValidatorOptions.Global.LanguageManager.Culture = new CultureInfo(language);
+
+app.Map("/lang", () => Results.Ok(TeamErrorsTest.Hello));
 
 app.UseCoreMiddlewares(config);
 app.MapControllers();
